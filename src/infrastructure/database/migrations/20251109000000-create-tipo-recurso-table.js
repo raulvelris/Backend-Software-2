c659668eea81b214e2cb54cdda20352549ba71e9
fernandoAPG <106409015+fernandoAPG@users.noreply.github.com>
2025-11-15 19:07:53 -0500

    tablas de recursos

---
+++ src/infrastructure/database/migrations/20251109000000-create-tipo-recurso-table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('TipoRecurso', {
+      tipo_recurso_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nombre: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('TipoRecurso');
+  }
+};
